78d77319aeabf490539cee15edd39c922a04615c
Dmitry Podgorniy <podgorniy.d@gmail.com>
2018-12-02 15:17:50 +0100

    Use pug for server templates

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -2,22 +2,6 @@ import {Express} from 'express';
 
 export function initRoutes(app: Express) {
     app.get('*', (req, res) => {
-        res.status(200)
-        res.send(`<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blank</title>
-    <link rel="stylesheet" href="/dist/index.css">
-    <script src="/dist/index.js"></script>
-</head>
-<body>
-    <h1>Hi there</h1>
-</body>
-</html>
-`)
+        res.render('default')
     })
 }
--- src/server/web.ts
+++ src/server/web.ts
@@ -2,9 +2,12 @@ import express = require('express')
 import {initMiddleware} from './middleware'
 import {initRoutes} from './routes'
 import {WEB_SERVER_PORT} from '../conf';
+import * as path from 'path';
 
 export async function startServer() {
     const app = express()
+    app.set('view engine', 'pug')
+    app.set('views', path.resolve(__dirname, 'views'))
     initMiddleware(app)
     initRoutes(app)
     app.listen(WEB_SERVER_PORT)
